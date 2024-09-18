ebea95d725abe40dbbf42a89423c24325e8f597d
kalai-17 <146472654+kalai-17@users.noreply.github.com>
2024-09-19 00:06:18 +0530

    Update register.php

--- register.php
+++ register.php
@@ -1,32 +1,28 @@
 <?php
-// Capture form data
+
 $firstname = $_POST["firstname"];
 $lastname = $_POST["lastname"];
 $email = $_POST["email"];
 $gender = $_POST["gender"];
 $phonenumber = $_POST["phonenumber"];
 $collegename = $_POST["collegename"];
-$course = $_POST["course"];
+$course = $_POST["Events"];
 
-// Create connection
-$conn = new mysqli('localhost', 'root', '1234', 'db_college_registration');
+$conn = new mysqli('localhost', 'root', '', 'db_event_registration');
 
-// Check connection
 if ($conn->connect_error) {
     die('Connection Failed: ' . $conn->connect_error);
 } else {
-    // Prepare and bind
-    $stmt = $conn->prepare("INSERT INTO registrations (firstname, lastname, email, gender, phonenumber, collegename, course) VALUES (?, ?, ?, ?, ?, ?, ?)");
-    $stmt->bind_param("sssssss", $firstname, $lastname, $email, $gender, $phonenumber, $collegename, $course);
-
-    // Execute statement
+    $stmt = $conn->prepare("INSERT INTO registration (firstname, lastname, email, gender, phonenumber, collegename, Events) VALUES (?, ?, ?, ?, ?, ?, ?)");
+    if ($stmt === false) {
+        die('Error preparing statement: ' . $conn->error); // Output SQL error
+    }
+    $stmt->bind_param("ssssiss", $firstname, $lastname, $email, $gender, $phonenumber, $collegename, $course);
     if ($stmt->execute()) {
         echo "Registration successful...";
     } else {
         echo "Error: " . $stmt->error;
     }
-
-    // Close statement and connection
     $stmt->close();
     $conn->close();
 }
